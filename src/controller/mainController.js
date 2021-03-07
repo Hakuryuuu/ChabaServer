e48b77f42d4a91e8cdc6acc3881a5a48ccb84247
DESKTOP-8AMRU42\OT <Peerapat.yo@rmuti.ac.th>
2021-03-07 19:06:23 +0700

    update import class

--- src/controller/mainController.js
+++ src/controller/mainController.js
@@ -1,3 +1,6 @@
+const sendLine = require('../constant/sendLine');
+const serviceLine = new sendLine();
+
 exports.main = async function (req, res) {
 	let reply_token = req.body.events[0].replyToken;
 	let msg = (req.body.events[0] && req.body.events[0].message.text) ? req.body.events[0].message.text : '';
