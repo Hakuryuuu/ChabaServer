89296a67ac3d9affb1d3a2cd90e5cc0027f8b46c
DESKTOP-8AMRU42\OT <Peerapat.yo@rmuti.ac.th>
2021-03-07 19:34:53 +0700

    update state

--- src/controller/mainController.js
+++ src/controller/mainController.js
@@ -2,18 +2,24 @@ const sendLine = require('../constant/sendLine');
 const serviceLine = new sendLine();
 
 exports.main = async function (req, res) {
-	console.log(req.body)
-	let reply_token = req.body.events[0].replyToken;
-	let msg = (req.body.events[0] && req.body.events[0].message.text) ? req.body.events[0].message.text : '';
-	let uuid = req.body.events[0].source.userId;
-	let body = [];
+    console.log(req.body);
+    let reply_token = req.body.events[0].replyToken;
+    let uuid = req.body.events[0].source.userId;
+    let msg = '';
+    let body = [];
 
-	// Test Return 
-	body.push(serviceLine.messageText(`${msg} - UUID ${uuid}`));
+    if (req.body.events[0].type == 'beacon') {
+        msg = 'Beacon';
+    } else {
+        msg = (req.body.events[0] && req.body.events[0].message.text) ? req.body.events[0].message.text : '';
+    }
 
-	if (body.length > 0) {
-		serviceLine.replyLine(reply_token, body);
-	}
+    // Test Return 
+    body.push(serviceLine.messageText(`${msg} - UUID ${uuid}`));
 
-	res.sendStatus(200);
+    if (body.length > 0) {
+        serviceLine.replyLine(reply_token, body);
+    }
+
+    res.sendStatus(200); 
 };
