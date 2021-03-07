2f96a61566319b4a3330ddb51709a609460f4698
DESKTOP-8AMRU42\OT <Peerapat.yo@rmuti.ac.th>
2021-03-07 19:12:51 +0700

    update debug requested body

--- src/controller/mainController.js
+++ src/controller/mainController.js
@@ -2,6 +2,7 @@ const sendLine = require('../constant/sendLine');
 const serviceLine = new sendLine();
 
 exports.main = async function (req, res) {
+	console.log(req.body)
 	let reply_token = req.body.events[0].replyToken;
 	let msg = (req.body.events[0] && req.body.events[0].message.text) ? req.body.events[0].message.text : '';
 	let uuid = req.body.events[0].source.userId;
