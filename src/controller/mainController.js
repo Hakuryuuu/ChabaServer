6a63461e9b4536a185c4f8a0351ff5d7cee62633
DESKTOP-8AMRU42\OT <Peerapat.yo@rmuti.ac.th>
2021-03-14 18:38:34 +0700

    remove uuid 2

--- src/controller/mainController.js
+++ src/controller/mainController.js
@@ -4,7 +4,7 @@ const serviceLine = new sendLine();
 exports.main = async function (req, res) {
     console.log(req.body);
     let reply_token = req.body.events[0].replyToken;
-    let uuid = req.body.events[0].source.userId;
+  //  let uuid = req.body.events[0].source.userId;
     let msg = '';
     let body = [];
 
@@ -13,13 +13,14 @@ exports.main = async function (req, res) {
     } else {
         msg = (req.body.events[0] && req.body.events[0].message.text) ? req.body.events[0].message.text : '';
     }
+}
 
     // Test Return 
-   
+ /*   body.push(serviceLine.messageText(`${msg} - UUID ${uuid}`));
 
     if (body.length > 0) {
         serviceLine.replyLine(reply_token, body);
     }
 
     res.sendStatus(200); 
-};
+}; */
