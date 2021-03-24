cd1a3e5b70f2ee62e971356cc6b83dce9067e391
DESKTOP-8AMRU42\OT <Peerapat.yo@rmuti.ac.th>
2021-03-24 23:21:55 +0700

    update code for HWID

--- src/controller/mainController.js
+++ src/controller/mainController.js
@@ -2,17 +2,28 @@ const sendLine = require('../constant/sendLine');
 const serviceLine = new sendLine();
 
 exports.main = async function (req, res) {
-    console.log(req.body.events[0].beacon);
+    console.log(req.body.events[0]);
     let reply_token = req.body.events[0].replyToken;
-    let beacon = req.body.events[0].beacon;
     let msg = '';
     let body = [];
+    let getHwid = req.body.events[0].beacon.hwid;
+    let locationLists =
+    {
+        "014ad71cfd": "ที่นี่ปราสาทศีขรภูมิ",
+        "014b374550": "ที่นี่วนอุทยานพนมสวาย"
+    };
 
-    if (req.body.events[0].type == 'beacon') {
-        msg = 'ที่นี่ปราสาทศีขรภูมิ';
-    } else {
-        msg = (req.body.events[0] && req.body.events[0].message.text) ? req.body.events[0].message.text : '';
-    }
+    Object.keys(locationLists).map( hwid => {
+        if (hwid == getHwid) {
+            msg = locationLists[hwid]
+        }
+    })
+
+    // if (req.body.events[0].type == 'beacon') {
+    //     msg = '';
+    // } else {
+    //     msg = (req.body.events[0] && req.body.events[0].message.text) ? req.body.events[0].message.text : '';
+    // }
 
 
     // Test Return 
